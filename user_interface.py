283efd5c9672943a06a767053766419afe0abe26
zwemster <104361935+zwemster@users.noreply.github.com>
2023-10-11 13:49:27 +0300

    added logger actions to UI file (user_choice method)

--- user_interface.py
+++ user_interface.py
@@ -1,4 +1,5 @@
 import shutil
+import logger
 
 
 def notes_cover():      # обложка приложения "заметки"
@@ -16,3 +17,25 @@ def notes_menu():       # меню заметок
     print('\t3. Прочитать заметку')
     print('\t4. Удалить заметку')
     print('\t5. Закрыть приложение')
+
+
+def user_choice():
+    notes_work = True
+    while notes_work:
+        choice = input('\n\tВыберите пункт меню: ')
+        if int(choice) == 1:
+            logger.logging.info('User selected item 1')
+        elif int(choice) == 2:
+            logger.logging.info('User selected item 2')
+        elif int(choice) == 3:
+            logger.logging.info('User selected item 3')
+        elif int(choice) == 4:
+            logger.logging.info('User selected item 4')
+        elif int(choice) == 5:
+            logger.logging.info('User selected item 5')
+            print('Закрытие приложения...')
+            logger.logging.info('Shutdown')
+            notes_work = False
+        else:
+            print('Вы ввели неверный пункт меню.')
+            logger.logging.info('User input an invalid value')
