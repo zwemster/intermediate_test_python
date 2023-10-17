2a169e848d13a8248ab69cbd7d53c2b7d2aebd43
zwemster <104361935+zwemster@users.noreply.github.com>
2023-10-17 20:47:48 +0300

    UI file was updated; main menu functions file was created

--- user_interface.py
+++ user_interface.py
@@ -2,15 +2,18 @@ import shutil
 import logger
 
 
-def notes_cover():      # обложка приложения "заметки"
-    notes_info = ['=' * 41, 'З А М Е Т К И', '=' * 41]
+app_name = ['=' * 41, 'З А М Е Т К И', '=' * 41]
+
+
+def notes_cover():          # обложка приложения "заметки"
     width = shutil.get_terminal_size().columns
-    position = (width - max(map(len, notes_info))) // 2
-    for info in notes_info:
+    position = (width - max(map(len, app_name))) // 2
+    for info in app_name:
         print(info.center(width))
 
 
-def notes_menu():       # меню заметок
+def notes_main_menu():      # главное меню заметок
+    print(app_name)
     print('\tМ Е Н Ю')
     print('\t1. Создать новую заметку')
     print('\t2. Редактировать существующую заметку')
@@ -19,20 +22,73 @@ def notes_menu():       # меню заметок
     print('\t5. Закрыть приложение')
 
 
+def notes_editor_submenu():        # подменю заметок
+    print(app_name)
+    print('Р Е Д А К Т И Р О В А Н И Е   З А М Е Т О К\t')
+    print('\t1. Показать все заметки')
+    print('\t2. Выполнить поиск по заголовку')
+    print('\t3. Выберите заметку для редактирования')
+
+
+def notes_reader_submenu():
+    print(app_name)
+    print('Ч Т Е Н И Е   З А М Е Т О К\t')
+    print('\t1. Показать все заметки')
+    print('\t2. Выполнить поиск по заголовку')
+    print('\t3. Выберите заметку')
+
+
+def notes_delete_submenu():
+    print(app_name)
+    print('У Д А Л Е Н И Е   З А М Е Т О К\t')
+    print('\t1. Показать все заметки')
+    print('\t2. Выберите заметку для удаления')
+
+
 def user_choice():
     notes_work = True
     while notes_work:
-        choice = input('\n\tВыберите пункт меню: ')
-        if int(choice) == 1:
-            logger.logging.info('User selected item 1')
-        elif int(choice) == 2:
-            logger.logging.info('User selected item 2')
-        elif int(choice) == 3:
-            logger.logging.info('User selected item 3')
-        elif int(choice) == 4:
-            logger.logging.info('User selected item 4')
-        elif int(choice) == 5:
-            logger.logging.info('User selected item 5')
+        main_menu_choice = input('\n\tВыберите пункт меню: ')
+        if int(main_menu_choice) == 1:
+            logger.logging.info('User selected main menu item 1')
+        elif int(main_menu_choice) == 2:
+            logger.logging.info('User selected main menu 2')
+            notes_editor_submenu()
+            editor_submenu_choice = int('\n\tВыберите пункт меню: ')
+            if int(editor_submenu_choice == 1):
+                logger.logging.info('User selected editor submenu item 1')
+            elif int(editor_submenu_choice == 2):
+                logger.logging.info('User selected editor submenu item 2')
+            elif int(editor_submenu_choice == 3):
+                logger.logging.info('User selected editor submenu item 3')
+            else:
+                print('Вы указали неверный пункт. Повторите попытку.')
+        elif int(main_menu_choice) == 3:
+            logger.logging.info('User selected main menu item 3')
+            notes_reader_submenu()
+            reader_submenu_choice = int('\n\tВыберите пункт меню: ')
+            if int(reader_submenu_choice == 1):
+                logger.logging.info('User selected reader submenu item 1')
+            elif int(reader_submenu_choice == 2):
+                logger.logging.info('User selected reader submenu item 2')
+            elif int(reader_submenu_choice == 3):
+                logger.logging.info('User selected reader submenu item 3')
+            else:
+                print('Вы указали неверный пункт. Повторите попытку.')
+        elif int(main_menu_choice) == 4:
+            logger.logging.info('User selected main menu item 4')
+            notes_delete_submenu()
+            delete_menu_choice = int("\n\tВыберите пункт меню: ")
+            if int(delete_menu_choice == 1):
+                logger.logging.info('User selected delete submenu item 1')
+            elif int(delete_menu_choice == 2):
+                logger.logging.info('User selected delete submenu item 2')
+            elif int(delete_menu_choice == 3):
+                logger.logging.info('User selected delete submenu item 3')
+            else:
+                print('Вы указали неверный пункт. Повторите попытку.')
+        elif int(main_menu_choice) == 5:
+            logger.logging.info('User selected main menu item 5')
             print('Закрытие приложения...')
             logger.logging.info('Shutdown')
             notes_work = False
