cf11008ef80e531e11a436b8c26905efe9184e82
zwemster <104361935+zwemster@users.noreply.github.com>
2023-10-17 20:59:26 +0300

    main_menu_functions.py: created method to read a list of notes

--- main_menu_functions.py
+++ main_menu_functions.py
@@ -11,3 +11,12 @@ def create_notes_file(file_path):
             csv_writer.writerow(['Name', 'Note', 'Date', 'Time'])
 
 
+def list_notes(file_path, filter_date=None):
+    notes = []
+    with open(file_path, 'r', newline='') as csvfile:
+        csv_reader = csv.reader(csvfile, delimiter=';')
+        notes = list(csv_reader)
+    if filter_date:
+        notes = [note for note in notes if note[2] == filter_date]
+    return notes
+
